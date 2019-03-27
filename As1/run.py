ae01b1bced55d37a7bb40d77a05e5e7a0a624578
Kostis-S-Z <kostis.s.z.24@gmail.com>
2019-03-27 12:24:21 +0100

    First commit for assignment 1

---
+++ As1/run.py
@@ -0,0 +1,17 @@
+"""
+Created by Kostis S-Z @ 2019-03-27
+"""
+
+
+def load_data():
+    """
+
+    :return: the preprocessed images and the labels of CIFAR-10
+    """
+
+
+def preprocess_data(data):
+    """
+    Preprocess data by normalizing between [0,1]
+    :return: the preprocessed data
+    """
